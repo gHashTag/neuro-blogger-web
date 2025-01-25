bde1875da69f219d4f49e0992feca32b70ba103e
Dmitriy Vasilev <raoffonom@icloud.com>
2025-01-25 11:57:14 +0700

    title color

--- src/pages/[username]/[user_id]/[workspace_id]/[room_id]/[recording_id]/[task_id]/[level]/levelBadge.tsx
+++ src/pages/[username]/[user_id]/[workspace_id]/[room_id]/[recording_id]/[task_id]/[level]/levelBadge.tsx
@@ -6,7 +6,7 @@ interface LevelBadgeProps {
 const LevelBadge: React.FC<LevelBadgeProps> = ({ level, is_ru }) => {
   return (
     <div className='absolute left-0 top-0 rounded-br-lg bg-white bg-opacity-30 p-4 shadow-md'>
-      <h2 className='text-sm font-bold tracking-wide text-black'>
+      <h2 className='text-sm font-bold tracking-wide text-white'>
         {is_ru ? 'УРОВЕНЬ АВАТАРА' : 'AVATAR LEVEL'} {level}
       </h2>
     </div>
